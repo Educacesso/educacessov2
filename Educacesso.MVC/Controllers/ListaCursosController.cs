49057134242b0d7e5e6e173b85965dc207847bf0
thiago901a <thiago19a@gmail.com>
2015-05-25 19:38:52 -0300

    Adicionado opcao para Ver cursos de quem o usuario segue

---
+++ Educacesso.MVC/Controllers/ListaCursosController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Educacesso.Domain.Entities;
+using Educacesso.MVC.ViewModels;
+using EducacessoV2.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Educacesso.MVC.Controllers
+{
+    public class ListaCursosController : Controller
+    {
+
+		private readonly ICursoAppService _cursoApp;
+
+		public ListaCursosController(ICursoAppService cursoApp)
+		{
+			_cursoApp = cursoApp;
+		}
+
+        //
+        // GET: /ListaCursos/
+        public ActionResult Index(string Id)
+        {
+			var cursoViewModel = Mapper.Map<IEnumerable<Curso>, IEnumerable<CursoViewModel>>(_cursoApp.CursosDoUsuarioLogado(Id));
+            return View(cursoViewModel);
+        }
+	}
+}
